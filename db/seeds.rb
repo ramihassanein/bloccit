3bfd80439e0fa75e7f36b907728071f8e944c058
ramihassanein <rami.hassanein09@gmail.com>
2015-05-21 19:12:12 +0000

    seed db has both contents

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+=begin
+
 if Post.where(title: "A test Post", body: "blah blah blah").blank?
   new_post=Post.create!(title: "A test Post", body: "blah blah blah")
 end
@@ -7,3 +9,27 @@ if Comment.where(body: "this is a comment").blank?
 end
 
 ## How to chech for a comment on a specific post ?!!
+=end
+
+require 'faker'
+
+50.times do
+  Post.create!(
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph
+    )
+end
+
+posts = Post.all
+
+100.times do 
+  Comment.create!(
+    post: posts.sample,
+    body: Faker::Lorem.paragraph
+    )
+end
+
+
+puts "Seed Finished"
+puts "Number of Posts = #{Post.count}"
+puts "Number of comments = #{Comment.count}"
